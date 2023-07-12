598fd8beb94894eb067929705179e5632f3c8370
kokoa322 <tjsgus223@gmailcom>
2023-07-12 18:02:49 +0900

    1. JPA 연관관계 설정
    2. h2로 확인

--- src/main/java/com/baesullin/pro/review/domain/Review.java
+++ src/main/java/com/baesullin/pro/review/domain/Review.java
@@ -1,5 +1,8 @@
 package com.baesullin.pro.review.domain;
 
+import com.baesullin.pro.store.domain.Store;
+import com.baesullin.pro.tag.domain.Tag;
+import com.baesullin.pro.user.domain.User;
 import com.baesullin.pro.util.TimeStamped;
 import lombok.*;
 
@@ -29,5 +32,38 @@ public class Review extends TimeStamped {
     @Column(nullable = false)
     private double point;
 
+
+    //1.@OneToMany(mappedBy = "userId") // 연관관계의 주인인 OrderItem의 userId 매핑 되어있다는 뜻
+
+    //2.JPA 영속성 전이(CASCADE)
+    //부모 엔티티가 영속화될 때 자식 엔티티도 같이 영속화되고, 부모 엔티티가 삭제될 때 자식 엔티티도 삭제되는 등 특정 엔티티를 영속 상태로 만들 때 연관된 엔티티도 함께 영속 상태로 전이되는 것을 의미
+
+    //CascadeType.ALL: 모든 Cascade를 적용
+    //CascadeType.PERSIST: 엔티티를 영속화할 때, 연관된 엔티티도 함께 유지
+    //CascadeType.MERGE: 엔티티 상태를 병합(Merge)할 때, 연관된 엔티티도 모두 병합
+    //CascadeType.REMOVE: 엔티티를 제거할 때, 연관된 엔티티도 모두 제거
+    //CascadeType.DETACH: 부모 엔티티를 detach() 수행하면, 연관 엔티티도 detach()상태가 되어 변경 사항 반영 X
+    //CascadeType.REFRESH: 상위 엔티티를 새로고침(Refresh)할 때, 연관된 엔티티도 모두 새로고침
+
+    //3.orpahnRemoval이란 고아 객체(Orphan)을 제거한다는 뜻으로, 부모 Entity와의 연관관계가 끊어진 자식 Entity를 자동으로 삭제하는 기능
+
+    //4.@ManyToOne(fetch = FetchType.LAZY) 참고로 @ManyToOne 매핑의 기본 fetch가 EAGER라서 생략해도 EAGER로 동작한다
+    // https://velog.io/@jin0849/JPA-%EC%A6%89%EC%8B%9C%EB%A1%9C%EB%94%A9EAGER%EA%B3%BC-%EC%A7%80%EC%97%B0%EB%A1%9C%EB%94%A9LAZY
+
+
+    //리뷰 이미지 URL
+    @OneToMany(mappedBy = "reviewId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STORE_ID", nullable = false)
+    private Store storeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User userId;
+
+    @OneToMany(mappedBy = "reviewId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tag> tagList = new ArrayList<>();
 
 }
--- src/main/java/com/baesullin/pro/tag/domain/Tag.java
+++ src/main/java/com/baesullin/pro/tag/domain/Tag.java
@@ -23,6 +23,10 @@ public class Tag extends TimeStamped {
     private String tag;
     //jpa를 리스트/JSON 집어넣는 방법?
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "REVIEW_ID", nullable = false)
+    private Review reviewId;
+
 
 }
 
