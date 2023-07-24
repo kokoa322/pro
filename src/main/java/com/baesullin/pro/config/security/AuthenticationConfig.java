db2242b55e4c7a6c055cd40bc51d83b0e2930a88
kokoa322 <tjsgus223@gmailcom>
2023-07-24 19:04:22 +0900

    1.gladle SpringSecurity 추가
    2.AuthenticationConfig 추가

--- src/main/java/com/baesullin/pro/config/security/AuthenticationConfig.java
+++ src/main/java/com/baesullin/pro/config/security/AuthenticationConfig.java
@@ -1,2 +1,31 @@
-package com.baesullin.pro.config.security;public class AuthenticationConfig {
+package com.baesullin.pro.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity //
+public class AuthenticationConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable() // 인증을 UI가 아니라 토큰인증으로..
+                .csrf().disable() // 모름
+                .cors().and()// 모름
+                .authorizeRequests() // request를 authorize 하겠다
+                .antMatchers("/review","/join", "/h2-console","/h2-console/login" ).permitAll() // 이 경로는 허용하겠다.
+                .antMatchers(HttpMethod.POST,"/*").authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 사용
+                .and()
+                //.addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
